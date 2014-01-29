da5859fc061bf066e147297aa4ecd31e51e2f013
cclaiborne <claiborne206@gmail.com>
2014-01-29 00:53:33 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Qasite::Application.config.secret_key_base = '494028735a4dce5814b5ebb53a62147e686edea26099c95ebeee17177a1a957dc71bd58b450c8dac2e247327602f516d84f03dd0c0bda2e2c76b5eb3e53840c5'
